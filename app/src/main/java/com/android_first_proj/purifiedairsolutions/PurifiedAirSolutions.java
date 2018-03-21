3b2d325c5d5eecfe749bc47f36daf146caa21aa7
Daniel McFarlin <danielmcfarlin7@gmail.com>
2018-03-21 18:08:01 -0500

    Initial commit

---
+++ app/src/main/java/com/android_first_proj/purifiedairsolutions/PurifiedAirSolutions.java
@@ -0,0 +1,13 @@
+package com.android_first_proj.purifiedairsolutions;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PurifiedAirSolutions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_purified_air_solutions);
+    }
+}
